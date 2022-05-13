1cf128e0d7bd3f9218c384141a566a80878ff175
Marcell Tanure <paganini.marcell@gmail.com>
2022-05-13 14:53:17 -0300

    Default test passing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders h1', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const h1Tag = screen.getByText(/Telecom Remote Monitoring/i);
+  expect(h1Tag).toBeInTheDocument();
 });
