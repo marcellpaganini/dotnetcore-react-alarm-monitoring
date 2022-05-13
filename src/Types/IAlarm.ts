f75ace68dc0bf07de8a6b14c99fca6465d2fd9bd
Marcell Tanure <paganini.marcell@gmail.com>
2022-05-13 15:58:14 -0300

    Ialarm created (issue with uuid)

---
+++ src/Types/IAlarm.ts
@@ -0,0 +1,11 @@
+import uuid from 'uuid';
+import { IAlarmInfo } from "./IAlarmInfo";
+
+export interface IAlarm {
+    id: uuid.V4Options();
+    name: string;
+    alarmInfo: IAlarmInfo;
+    firstReceiveDate: Date;
+    clearedDate: Date;
+    duration: number;
+} 
