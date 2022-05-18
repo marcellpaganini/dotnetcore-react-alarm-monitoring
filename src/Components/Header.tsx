f8af11736bb5f94c0fb5e2ba195061517885422c
Marcell Tanure <paganini.marcell@gmail.com>
2022-05-18 09:45:13 -0300

    Severity count implemented

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -10,9 +10,29 @@ export const Header: FC<HeaderProps> = (props: HeaderProps) => {
   return (
     <div className="header">
         <h1>📡</h1>
+
         <section className="active">
-            <span>Active Alarms</span><br />
+            <span className="activeTitle">Active Alarms</span><br />
             <span className="activeCount">{props.alarms.length}</span>
+        </section>
+
+        <section className="severity">
+          <section className="criticalCount">
+            <span className="criticalNumber">{props.alarms.filter(a => a.alarmInfo.severity === 3).length}</span>
+            <span className="criticalDescription">Critical</span>
+          </section>
+          <section className="majorCount">
+            <span className="majorNumber">{props.alarms.filter(a => a.alarmInfo.severity === 2).length}</span>
+            <span>Major</span>
+          </section>
+          <section className="minorCount">
+            <span className="minorNumber">{props.alarms.filter(a => a.alarmInfo.severity === 1).length}</span>
+            <span>Minor</span>
+          </section>
+          <section className="warningCount">
+            <span className="warningNumber">{props.alarms.filter(a => a.alarmInfo.severity === 0).length}</span>
+            <span>Warning</span>
+          </section>
         </section>
     </div>
   )
