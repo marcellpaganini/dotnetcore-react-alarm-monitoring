0859d2fb1b19ee66d4fbddd019b24934589f539c
Marcell Tanure <paganini.marcell@gmail.com>
2022-06-01 13:33:26 -0300

    User registration implemented

--- Client/src/Components/Auth/Register.tsx
+++ Client/src/Components/Auth/Register.tsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { SyntheticEvent, useState } from 'react';
+import { Navigate } from 'react-router-dom';
 import './SignIn.css';
 
 type Props = {}
 
 export const Register = (props: Props) => {
+  const [userName, setUserName] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [redirect, setRedirect] = useState<boolean>(false);
+
+  const submit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    await fetch('http://localhost:8000/api/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/JSON' },
+      body: JSON.stringify({
+        userName,
+        password
+      })
+    });
+
+    setRedirect(true);
+  };
+
+  if(redirect) {
+    return <Navigate to='/signin' />;
+  }
+
   return (
-    <div className="signIn"> 
-      <section className="signInForm">
-          <h1>Please Register</h1>
-          <input type="text" placeholder="User Name" required></input>
-          <input type="password" placeholder="Password" required></input>
-          <button type="submit">Submit</button>
-      </section>
-    </div> 
+      <div className="signIn"> 
+        <section className="signInForm">
+          <form onSubmit={submit}>
+            <h1>Please Register</h1>
+            <input type="text" placeholder="User Name" required onChange={e => setUserName(e.target.value)}></input>
+            <input type="password" placeholder="Password" required onChange={e => setPassword(e.target.value)}></input>
+            <button type="submit">Submit</button>
+          </form>
+        </section>
+      </div>   
   )
 }
--- Client/src/Components/Auth/SignIn.tsx
+++ Client/src/Components/Auth/SignIn.tsx
@@ -7,10 +7,12 @@ export const SignIn = (props: Props) => {
   return (
     <div className="signIn"> 
       <section className="signInForm">
+        <form>
           <h1>Please Sign In</h1>
           <input type="text" placeholder="User Name" required></input>
           <input type="password" placeholder="Password" required></input>
           <button type="submit">Sign In</button>
+        </form>
       </section>
     </div> 
   )
