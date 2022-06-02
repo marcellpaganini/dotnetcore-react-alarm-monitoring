55cfcb9e65f7e22e4dff958c03a6341579a072b9
Marcell Tanure <paganini.marcell@gmail.com>
2022-06-02 10:01:42 -0300

    Sign in validation implemented

--- Client/src/Components/Auth/SignIn.tsx
+++ Client/src/Components/Auth/SignIn.tsx
@@ -8,11 +8,13 @@ export const SignIn = (props: Props) => {
   const [userName, setUserName] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [redirect, setRedirect] = useState<boolean>(false);
+  const [name, setName] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
 
   const submit = async (e: SyntheticEvent) => {
     e.preventDefault();
 
-    await fetch('http://localhost:8000/api/login', {
+    const response = await fetch('http://localhost:8000/api/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/JSON' },
       credentials: 'include',
@@ -22,7 +24,14 @@ export const SignIn = (props: Props) => {
       })
     });
 
-    setRedirect(true);
+    const content = await response.json();
+    setName(content.userName);
+
+    if (name !== "") {
+      setRedirect(true);
+    } else {
+      setMessage("Invalid user name or password.");
+    }
   };
 
   if(redirect) {
@@ -35,6 +44,7 @@ export const SignIn = (props: Props) => {
           <h1>Please Sign In</h1>
           <input type="text" placeholder="User Name" required onChange={e => setUserName(e.target.value)}></input>
           <input type="password" placeholder="Password" required onChange={e => setPassword(e.target.value)}></input>
+          <span className='error'>{message}</span>
           <button type="submit">Sign In</button>
         </form>
       </section>
