96fc19cf744a1067b7c12945d04ff18bbf4c21fd
Marcell Tanure <paganini.marcell@gmail.com>
2022-06-02 13:30:42 -0300

    Front-end validation implemented

--- Client/src/Components/Auth/Register.tsx
+++ Client/src/Components/Auth/Register.tsx
@@ -8,9 +8,24 @@ export const Register = (props: Props) => {
   const [userName, setUserName] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [redirect, setRedirect] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+
+  const checkPassword = (password: string): boolean => {
+    var pattern = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$");  
+    if (password.length >= 7 && pattern.test(password)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   const submit = async (e: SyntheticEvent) => {
     e.preventDefault();
+
+    if (!checkPassword(password) || userName.length < 2) {
+      setError("Registration requires: Password with at least 7 characters, uppercase, lowercase letter, number, special character and user name with at least 2 characters.");
+      return;
+    }
 
     await fetch('http://localhost:8000/api/register', {
       method: 'POST',
@@ -36,6 +51,7 @@ export const Register = (props: Props) => {
             <input type="text" placeholder="User Name" required onChange={e => setUserName(e.target.value)}></input>
             <input type="password" placeholder="Password" required onChange={e => setPassword(e.target.value)}></input>
             <button type="submit">Submit</button>
+            <span className="error">{error}</span>
           </form>
         </section>
       </div>   
